aeff00f1783b99b47cb923dbe14df3dadb4237f4
qamarabdiaziz <tushez98@gmail.com>
2023-03-20 12:18:41 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+let num1 = 31
+let num2 = 2
+let multiply= (num1 * num2)
+console.log (multiply)
+console.log(multiply);
+
+let random =Math.floor(Math.random()*10)+1 ;
+console.log(random)
+
+let num3=20
+let num4=8
+let mod =num%num4
+console.log(mod)
+
+let max= (1,2,4,8,20)
+console.log(max)
+
